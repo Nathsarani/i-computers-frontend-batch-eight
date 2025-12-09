fa8187a86fc387ef41674067b70f7d987eeeadf4
Nathsarani <prabhashi@gmail.com>
2025-12-09 21:31:58 +0500

    Day 14 🖍

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Test from './components/test'
+
 
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import RegisterPage from './pages/registerPage'
 import AdminPage from './pages/adminPage'
 import TestPage from './pages/test'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
@@ -16,9 +17,12 @@ function App() {
 
     <BrowserRouter>
 
+      <Toaster position='top-right'/>
+
         <div className=' w-full h-screen border-2'    /* hema page ekakama container  */> 
                   <div  className='w-full h-screen bg-primary text-secondary'/*container eke redda eka wenas wenwa screen eka anuwa(dynamic kotuwak admin,re,vge page wetena*/> 
-                      <Routes path="/">
+                      <Routes>
+            
                           <Route path="/*" element={<HomePage/>}/>
                           <Route path="/" element={<HomePage/>}/>
                           <Route path="/login" element={<LoginPage/>}/>
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,6 +1,100 @@
-export default function loginPage(){
-    return(
-        <h1>Login Page</h1>
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+
+export default function loginPage() {
+    const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+    
+    async function login(){
+
+        console.log("Login button clicked");
+        console.log("Email:",email);
+        console.log("Password:",password);
+
+        try{
+
+        const res = await axios.post(import.meta.env.VITE_BACKEND_URL+ "/users/login",{ 
+            email : email, password : password 
+        });
+
+
+        console.log(res)
+        if (res.data.role == "admin") { 
+				//window.location.href = "/admin";
+                 navigate("/admin")
+		} else {
+				//window.location.href = "/";
+                 navigate("/admin")
+
+		}
+
+		
+        
+        //alert("Login successful! Welcome back.");
+        toast.success("Login successful! Welcome back.")
+
+        
+        }catch(error){
+
+             //alert("Login failed! Please check your credentials and try again.");
+            toast.error("Login failed! Please check your credentials and try again.");
+            console.log("Error during login");
+            console.log("FInd error",error);
+        }  
+        
+    }
+
+    return (
+        <div className="w-full h-screen bg-[url('/bg.jpg')] bg-center bg-cover bg-no-repeat flex">
+            <div className="w-[50%] h-full flex justify-center items-center flex-col">
+                <img src="/logo.png" alt="logo" className="w-[400px] h-[200px] mb-[20px] object-cover" />
+                <h1 className="text-4xl text-accent text-shadow-2xs font-bold text-center"> Plug In to Better Performance.</h1>
+                <h1 className="text-2xl text-secondary text-shadow-2xs italic text-center">High-quality accessories for work, gaming, and everyday use.</h1>
+
+            </div>
+
+            <div className="w-[50%] h-full flex justify-center items-center ">
+                <div className="w-[450px] h-[600px]  backdrop-blur-2xl shadow-2xl rounded-2xl flex flex-col justify-center items-center p-[30px] ">
+                    
+                    <h1 className="text-[40px] font-bold mb-[50px] text-accent text-shadow-2xs text-shadow-white">Login</h1>
+                    
+                    <input 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        type="email"
+                        placeholder="Enter your email"
+                        className="w-full h-[50px] mb-[20px] border rounded-lg border-secondary p-[10px] text-[20px] focus:outline-none focus:ring-2 focus:ring-accent" />
+                    
+                    <input 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        type="password"
+                        placeholder="your password" 
+                        className="w-full h-[50px] border rounded-lg border-secondary p-[10px] text-[20px] focus:outline-none focus:ring-2 focus:ring-accent" />
+
+                          
+                    
+                    <p className=" text-secondary  text-right  w-full mb-[20px] ">
+                        Forgot your password? <Link to="/forget-password" className="text-accent italic">
+                        Reset it here</Link></p>
+                    
+                    <button onClick={login} className="w-[350px] h-[50px]  border rounded-lg border-secondary  text-[20px] font-bold  text-white bg-accent hover:bg-transparent  hover:text-secondary">
+                        Login</button>
+
+                    <p className="text-secondary not-italic">Don't have account?
+                        <Link to="/register" className="text-accent italic "> Register here</Link>
+                    </p>
+
+                </div>
+
+
+
+
+
+            </div>
+        </div>
     )
 
 }
