4dc6e99e5c1b4e1e0dd47cce05fbbcc75f535f03
Nathsarani <prabhashi@gmail.com>
2025-12-16 14:11:59 +0500

    Day 19 🖍

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,13 +2,15 @@ import Header from "../components/header"
 import { Routes,Route } from "react-router-dom"
 import ProductPage from "./productPage"
 import ProductOverview from "./productOverview"
+import CartPage from "./cart"
+import CheckoutPage from "./checkOut"
 
 
 export default function HomePage(){
     return(
         <div className="w-full h-full overflow-y-scroll max-h-full">
             <Header/>
-            <div className="w-full min-h-[calc(100%-100px)] text-3xl">
+            <div className="w-full min-h-[calc(100%-100px)] text-3xl ">
             
                             <Routes>
                                       <Route path="/" element={<h1>Home Page</h1>}/>
@@ -16,6 +18,8 @@ export default function HomePage(){
                                       <Route path="/about" element={<h1>About</h1>}/>
                                       <Route path="/contact" element={<h1>Contact</h1>}/>
                                       <Route path="/overview/:productID" element={<ProductOverview/>}/>
+                                      <Route path="/cart" element={<CartPage/>}/>
+                                      <Route path="/checkout" element={<CheckoutPage/>}/>
                                       <Route path="/*" element={<h1>Not Found</h1>}/>
                                       
                             </Routes>    
